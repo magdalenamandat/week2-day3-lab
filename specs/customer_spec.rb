5fa81d546d35352d762c9f7f22e5615e56b3b390
Magdalena <m.mandat@gmail.com>
2018-05-30 15:24:13 +0100

    small letters

--- specs/customer_spec.rb
+++ specs/customer_spec.rb
@@ -6,56 +6,56 @@ require_relative("../food.rb")
 
 class CustomerTest < MiniTest::Test
 def setup
-  @Will = Customer.new("Will", 100, 30, 50)
-  @Dave = Customer.new("Dave", 200, 27, 30)
-  @Ben = Customer.new("Ben", 100, 15, 0)
-  @Joe = Customer.new("Joe", 50, 30, 30)
-  @Scott = Customer.new("Scott", 300, 19, 120)
-  @Gin = Drink.new("Gin", 10, 10)
-  @Beer = Drink.new("Beer", 5, 5 )
-  @Wine = Drink.new("Wine", 15, 10)
-  @Vodka = Drink.new("Vodka", 20, 15)
-  @Chips = Food.new("Chips", 5, 10)
-  @Pasta = Food.new("Pasta", 10, 20)
-  @Salad = Food.new("Salad", 15, 5)
-  @Chanter = Pub.new("Chanter", 100, [@Gin, @Beer, @Wine, @Vodka], [@Chips, @Pasta, @Salad])
+  @will = Customer.new("Will", 100, 30, 50)
+  @dave = Customer.new("Dave", 200, 27, 30)
+  @ben = Customer.new("Ben", 100, 15, 0)
+  @joe = Customer.new("Joe", 50, 30, 30)
+  @scott = Customer.new("Scott", 300, 19, 120)
+  @gin = Drink.new("Gin", 10, 10)
+  @beer = Drink.new("Beer", 5, 5 )
+  @wine = Drink.new("Wine", 15, 10)
+  @vodka = Drink.new("Vodka", 20, 15)
+  @chip = Food.new("Chips", 5, 10)
+  @pasta = Food.new("Pasta", 10, 20)
+  @salad = Food.new("Salad", 15, 5)
+  @chanter = Pub.new("Chanter", 100, [@gin, @beer, @wine, @vodka], [@chip, @pasta, @salad])
 end
 
 def test_customer_name
-  assert_equal("Will", @Will.name)
+  assert_equal("Will", @will.name)
 end
 
 def test_buy_drink__over_18
-  @Will.buy_a_drink(@Chanter, @Beer)
-  assert_equal(95, @Will.wallet)
-  assert_equal(105, @Chanter.till)
+  @will.buy_a_drink(@chanter, @beer)
+  assert_equal(95, @will.wallet)
+  assert_equal(105, @chanter.till)
 end
 
 def test_buy_drink__under_18
-  @Ben.buy_a_drink(@Chanter, @Beer)
-  assert_equal(100, @Ben.wallet)
-  assert_equal(100, @Chanter.till)
+  @ben.buy_a_drink(@chanter, @beer)
+  assert_equal(100, @ben.wallet)
+  assert_equal(100, @chanter.till)
 end
 
 def test_buy_drink__too_drunk
-  @Scott.buy_a_drink(@Chanter, @Beer)
-  assert_equal(300, @Scott.wallet)
-  assert_equal(100, @Chanter.till)
-  assert_equal(120, @Scott.drunkenness)
+  @scott.buy_a_drink(@chanter, @beer)
+  assert_equal(300, @scott.wallet)
+  assert_equal(100, @chanter.till)
+  assert_equal(120, @scott.drunkenness)
 end
 
 def test_buy_drink__not_drunk
-  @Joe.buy_a_drink(@Chanter, @Gin)
-  assert_equal(40, @Joe.wallet)
-  assert_equal(110, @Chanter.till)
-  assert_equal(40, @Joe.drunkenness)
+  @joe.buy_a_drink(@chanter, @gin)
+  assert_equal(40, @joe.wallet)
+  assert_equal(110, @chanter.till)
+  assert_equal(40, @joe.drunkenness)
 end
 
 def test_buy_food
-  @Will.buy_food(@Chanter, @Chips)
-  assert_equal(95, @Will.wallet)
-  assert_equal(105, @Chanter.till)
-  assert_equal(40, @Will.drunkenness)
+  @will.buy_food(@chanter, @chip)
+  assert_equal(95, @will.wallet)
+  assert_equal(105, @chanter.till)
+  assert_equal(40, @will.drunkenness)
 end
 
 
--- specs/pub_spec.rb
+++ specs/pub_spec.rb
@@ -5,32 +5,32 @@ require_relative("../pub.rb")
 class PubTest < MiniTest::Test
 
   def setup
-    @Gin = Drink.new("Gin", 10, 40)
-    @Beer = Drink.new("Beer", 5, 5)
-    @Wine = Drink.new("Wine", 15, 10 )
-    @Vodka = Drink.new("Vodka", 20, 45)
-    @Chanter = Pub.new("Chanter", 100, [@Gin, @Beer, @Wine, @Vodka], [])
+    @gin = Drink.new("Gin", 10, 40)
+    @beer = Drink.new("Beer", 5, 5)
+    @wine = Drink.new("Wine", 15, 10 )
+    @vodka = Drink.new("Vodka", 20, 45)
+    @chanter = Pub.new("Chanter", 100, [@gin, @beer, @wine, @vodka], [])
   end
 
   def test_get_pub_name
-    assert_equal("Chanter", @Chanter.name)
+    assert_equal("Chanter", @chanter.name)
   end
 
   def test_add_money()
-    @Chanter.add_money(10)
-    assert_equal(110, @Chanter.till)
+    @chanter.add_money(10)
+    assert_equal(110, @chanter.till)
   end
 
   def test_number_of_drinks
-    assert_equal(4, @Chanter.drinks_count())
+    assert_equal(4, @chanter.drinks_count())
   end
 
   def test_check_till_balance
-    assert_equal(100, @Chanter.till())
+    assert_equal(100, @chanter.till())
   end
 
   def test_check_total_stock_value
-    assert_equal(50, @Chanter.check_total_stock_value)
+    assert_equal(50, @chanter.check_total_stock_value)
   end
 
 
