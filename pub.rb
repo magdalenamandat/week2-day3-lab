f5a8e48b5ac89fa16484b3b6d9eb7ccd038c3d9f
Magdalena <m.mandat@gmail.com>
2018-05-30 11:25:37 +0100

    added get name method

--- pub.rb
+++ pub.rb
@@ -5,4 +5,13 @@ def initialize(name, till, drinks)
   @till = till
   @drinks = drinks
 end
+
+def get_name()
+  return @name
+end
+
+def drinks_count()
+  return @drinks.length
+end
+
 end
--- specs/pub_spec.rb
+++ specs/pub_spec.rb
@@ -4,4 +4,16 @@ require_relative("../pub.rb")
 
 class PubTest < MiniTest::Test
 
+def setup
+  @Chanter = Pub.new("Chanter", 100, [])
+end
+
+def test_get_pub_name
+  assert_equal("Chanter", @Chanter.get_name)
+end
+
+def test_number_of_drinks
+  assert_equal(0, @Chanter.drinks_count())
+end
+
 end
